e23c3a9a1e67a635435113bc99d915d2701e925a
meow0102 <ninoatoian@gmail.com>
2024-05-24 20:05:56 +0200

    update

--- proj.js
+++ proj.js
@@ -1,9 +1,7 @@
 const listEl = document.querySelector(".whyAro-list");
-console.log(listEl);
 let currentActive = null;
 listEl.addEventListener("click", (event) => {
   if (event.target === event.currentTarget) {
-    console.log("click on list");
     return;
   }
   const liEl = event.target.closest(".whyAro-list-item");
@@ -16,6 +14,4 @@ listEl.addEventListener("click", (event) => {
   }
   liEl.classList.add("active");
   currentActive = liEl;
-
-  console.log(event.target.closest(".whyAro-list-item"));
 });
